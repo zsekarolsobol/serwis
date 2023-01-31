ff68083c2beb800743a2251301d6d771376973e8
Karol Sobolewski <zsekarolsobol@o2.pl>
2023-01-31 13:24:19 +0100

    dodanie metody getMapping oraz PostMapping dla User

--- src/main/java/pl/sobolewski/serwis/UserRepository.java
+++ src/main/java/pl/sobolewski/serwis/UserRepository.java
@@ -1,4 +1,6 @@
 package pl.sobolewski.serwis;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
---
+++ src/main/java/pl/sobolewski/serwis/UserService.java
@@ -0,0 +1,33 @@
+package pl.sobolewski.serwis;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @GetMapping("/users")
+    public ResponseEntity getUsers() throws JsonProcessingException {
+        List<User> allUsers = userRepository.findAll();
+        return ResponseEntity.ok(objectMapper.writeValueAsString(allUsers));
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity addUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.ok(savedUser);
+
+
+    }
+}
